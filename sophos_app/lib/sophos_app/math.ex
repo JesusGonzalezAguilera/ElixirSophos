d6be97b4f7cbc61a091b5799e814d4c5822ac905
JesusGonzalezAguilera <108531728+JesusGonzalezAguilera@users.noreply.github.com>
2022-08-09 10:54:20 -0500

    Update math.ex

--- sophos_app/lib/sophos_app/math.ex
+++ sophos_app/lib/sophos_app/math.ex
@@ -1,11 +1,30 @@
 defmodule SophosApp.Math do
+
+  @moduledoc """
+  Implementación básica del módulo de matemáticas de Sophos
+  """
+
+  @pi 3.1415
+
+  @doc """
+  Suma dos números
+  """
   def sum(a,b) do
     a + b
   end
+
+  def circumference(r), do: 2 * r *  @pi
+
+  @doc """
+  Calcula el Máximo Común Divisor
+  """
   def mcd(a,0), do: a
   def mcd(a,b), do: mcd(b, rem(a,b))
+
   defmodule Geometry.Rectangle do
     def area(a,b), do: a*b
     def area(a), do: area(a,a)
-end
+  end
+
+
 end
