d5f0ad590592aa21627027c71ffd54584566d821
JesusGonzalezAguilera <108531728+JesusGonzalezAguilera@users.noreply.github.com>
2022-08-09 10:16:39 -0500

    Update fizzbuzz.ex

--- sophos_app/lib/sophos_app/fizzbuzz.ex
+++ sophos_app/lib/sophos_app/fizzbuzz.ex
@@ -1,6 +1,9 @@
 defmodule SophosApp.Fizzbuzz do
   alias SophosApp.MyList
 
+  @moduledoc """
+  Implementación de FizzBuzzz
+  """
   def exec(n) do
     MyList.generate(n)
     |> gen_fizz_buzz()
@@ -10,12 +13,14 @@ defmodule SophosApp.Fizzbuzz do
     gen_fizz_buzz(list,[])
   end
 
-
+  @doc """
+  Guardas que valida los divisores comunes de fizzbuzz
+  """
   defp gen_fizz_buzz([],list), do: list
   defp gen_fizz_buzz([h|t],list) when rem(h,3) == 0 and rem(h,5) == 0, do: gen_fizz_buzz(t,list++[:fizzbuzz])
   defp gen_fizz_buzz([h|t],list) when rem(h,3) == 0, do: gen_fizz_buzz(t,list++[:fizz])
   defp gen_fizz_buzz([h|t],list) when rem(h,5) == 0, do: gen_fizz_buzz(t,list++[:buzz])
   defp gen_fizz_buzz([h|t],list), do: gen_fizz_buzz(t,list++[h])
 
-
 end
+
