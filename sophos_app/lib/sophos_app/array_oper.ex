5dd640adffd7a91f243b910360fc90e87f1c401e
JesusGonzalezAguilera <108531728+JesusGonzalezAguilera@users.noreply.github.com>
2022-08-09 10:14:19 -0500

    Update array_oper.ex

--- sophos_app/lib/sophos_app/array_oper.ex
+++ sophos_app/lib/sophos_app/array_oper.ex
@@ -1,13 +1,17 @@
 defmodule SophosApp.ArrayOper do
+
+  @moduledoc """
+  Implementación de funciones en array
+  """
   def exec(list) do
     arrayOper(list,[])
   end
 
+  defp arrayOper([],list), do: list
   defp arrayOper([h|t],[]) do
     oper = &(&1*&1)
     arrayOper(t, []++[oper.(h)])
   end
-  defp arrayOper([],list), do: list
   defp arrayOper([h|t],list) do
     oper = &(&1*&1)
     arrayOper(t, list++[oper.(h)])
